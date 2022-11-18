6a0e78e977beba76988dc8d1bb417af7ec2e4ee9
Eduardo Marques <programdor.eduardo@gmail.com>
2022-11-18 06:46:28 -0300

    counter added

--- src/components/Pomodoro-timer.tsx
+++ src/components/Pomodoro-timer.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useInterval } from "../hooks/use-interval";
 
 interface Props{
     defaultPomodoroTime:number
 }
 
 export function PomodoroTimer(props:Props){
-    return <div>Olá mundo</div>
+const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime)
+
+useInterval(() => {
+    setMainTime(mainTime - 1)
+}, 1000)
+
+    return <div>Olá mundo {mainTime}</div>
 }
